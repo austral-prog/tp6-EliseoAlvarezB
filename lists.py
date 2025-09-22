dde2bbca3986ee67a4cd376710704c69da8fff42
EliseoAlvarezB <ealvarezbrescia@mail.austral.edu.ar>
2025-09-21 23:54:12 -0300

    Add files via upload

--- lists.py
+++ lists.py
@@ -1,20 +1,48 @@
-# Replace the "ANSWER HERE" with your answer
+list = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+def remove_elements(list):
+    if len(list) == 6:
+        list.pop(5)
+        list.pop(4)
+        list.pop(0)
+        return list
+    elif len(list) == 5:
+        list.pop(4)
+        list.pop(0)
+        return list
+    elif len(list) > 0:
+        list.pop(0)
+        return list
+    
+    return list 
+    
+list = ['Red', 'Green', 'White', 'Black']
+def add_elements(list):
+    listLen = len(list)
+    list.insert(0, "Pink")
+    list.insert(listLen+1, "Yellow") # Tambien se puede hacer con list.append(yellow)
+    return list
 
-def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+list = ['Black', 'Pink', 'Yellow', 'Red', 'Green', 'White']
+list2 = ['Red', 'Green', 'Yellow', 'White', 'Black', 'Pink']
+def is_empty(list):
+    if len(list) == 0:
+        return True
+    else:
+        return False
+def check_lists(list, list2):
+    if len(list) >= 3 and len(list2) >= 3:
+        if list[2] == list2[2]:
+            return True
+        else:
+            return False
 
+list = [[1, 2, 3], [4, 5, 6, 7, 8], [9, 10, 11, 12]]
+def list_of_lists(list):
+    newList = [[],[],[]]
+    
+    newList[0] = (list[0][0:2])
+    newList[1] = (list[1][1:4])
+    newList[2] = (list[2][-2:])
 
-def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
-
-
-def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
-
-
-def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
-
-
-def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+    return newList
+list_of_lists(list)
